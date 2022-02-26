acb9d8f5ff4e009432094be0894dd1a826631fce
dimple jha <dimplejha98@gmail.com>
2022-02-26 21:18:18 +0530

    mongodb assigment0

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://dimplejha_17:<osVxDHqCJxxpeWGN>@cluster0.ut3on.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
+router.post("/createUser", async function (req, res) {
+    let data= req.body
+    let savedData= await UserModel.create(data)
+    res.send({msg: savedData})
 })
-
-router.post("/createUser", UserController.createUser  )
-
-router.get("/getUsersData", UserController.getUsersData)
-
+router.get("/getUserData", async function (req, res) {
+    let allUsers= await UserModel.find()
+    res.send({msg: allUsers})
+  //users
+})
 module.exports = router;
