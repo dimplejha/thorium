df5c1cc413ac5ae425516fae00e463779fc6888d
dimple jha <dimplejha98@gmail.com>
2022-03-08 09:42:25 +0530

    4march assigment/weekand assigment

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
+const {default:mongoose}=require("mongoose")
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
-
 app.use('/', route);
 
+mongoose.connect("mongodb+srv://shrati:65FywNUvGdDH49SQ@cluster0.tpfb4.mongodb.net/Shrati12",{
+    useNewUrlParser:true
+})
+.then(()=>console.log("mongodb is conncted"))
+.catch(err=>console.log(err))
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
